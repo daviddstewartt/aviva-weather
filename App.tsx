7ca45c88cf899878619e4f5fe4f266d52e525198
David Stewart <davidd.cc.stewartt@gmail.com>
2023-07-07 15:21:11 +0100

    Added: Current Route to Route Toggle Overlay

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
@@ -13,20 +13,23 @@ import {GestureHandlerRootView} from 'react-native-gesture-handler';
 const Stack = createStackNavigator();
 
 const App = () => {
+  const [currentRouteName, setCurrentRouteName] = useState();
   const defaultScreenOptions = {
     headerShown: false,
   };
 
   return (
     <GestureHandlerRootView style={{flex: 1}}>
-      <NavigationContainer>
+      <NavigationContainer
+        onStateChange={state =>
+          setCurrentRouteName(state?.index === 0 ? 'Home' : 'Forecast')
+        }>
         <Stack.Navigator screenOptions={defaultScreenOptions}>
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Forecast" component={ForecastScreen} />
         </Stack.Navigator>
+        <ForecastToggleOverlay currentRoute={currentRouteName || 'Home'} />
       </NavigationContainer>
-
-      <ForecastToggleOverlay />
     </GestureHandlerRootView>
   );
 };
