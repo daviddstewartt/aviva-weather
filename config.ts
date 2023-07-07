c0cd8dd0b3e1a8d6271fe3d34086ec35e4b48fa0
David Stewart <davidd.cc.stewartt@gmail.com>
2023-07-07 23:30:07 +0100

    Added: Ability To Search Cities

---
+++ config.ts
@@ -0,0 +1,6 @@
+const Config = {
+  // Would store these in a .env file in a real app
+  OPEN_WEATHER_MAP_API_KEY: 'adbc23ff4aa3c7a852a907602eac1249',
+};
+
+export {Config};
--- src/Components/SearchLocationsInput.tsx
+++ src/Components/SearchLocationsInput.tsx
@@ -6,8 +6,12 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
+
 import React, {useEffect, useState} from 'react';
+import {Config} from '../../config';
 import {Colors, Metrics} from '../theme';
+import {searchLocationCities} from '../util/location';
+import {ICity} from '../ts/interfaces';
 
 type SearchLocationsInputProps = {
   onSearchResultsVisibility: (visible: boolean) => void;
@@ -16,8 +20,10 @@ type SearchLocationsInputProps = {
 const SearchLocationsInput: React.FC<SearchLocationsInputProps> = ({
   onSearchResultsVisibility,
 }) => {
-  const [searchLocationString, setSearchLocationString] = useState<string>('');
+  const [citiesList, setCitiesList] = useState<ICity[]>([]);
   const [isSearchLoading, setIsSearchLoading] = useState<boolean>(false);
+  const [showCitiesList, setShowCitiesList] = useState<boolean>(false);
+  const [searchLocationString, setSearchLocationString] = useState<string>('');
 
   // Debounce the search string
   useEffect(() => {
@@ -26,13 +32,51 @@ const SearchLocationsInput: React.FC<SearchLocationsInputProps> = ({
     }
     setIsSearchLoading(true);
     const searchTimeoutId = setTimeout(() => {
-      // Call the API
+      getCitiesListByString(searchLocationString);
     }, 2000);
 
     return () => {
       clearTimeout(searchTimeoutId);
     };
   }, [searchLocationString]);
+
+  /**
+   * Gets the list of cities from the OpenWeatherMap API
+   * @param {string} location - The location to search for
+   */
+  const getCitiesListByString = async (location: string) => {
+    try {
+      const cities = await searchLocationCities(
+        location,
+        Config.OPEN_WEATHER_MAP_API_KEY,
+      );
+
+      console.log('Response:', cities);
+      setCitiesList(cities);
+      setShowCitiesList(true);
+      setIsSearchLoading(false);
+      onSearchResultsVisibility(true);
+    } catch (error) {
+      console.log('Error:', error.message);
+      setShowCitiesList(false);
+      setIsSearchLoading(false);
+      onSearchResultsVisibility(true);
+    }
+  };
+
+  /**
+   * Selects a city from the list of cities
+   * @param {ICity} city - The city to select
+   * TODO: Add the city to the store
+   */
+  const onSelectCity = (city: ICity): void => {
+    onSearchResultsVisibility(false);
+    setShowCitiesList(false);
+    setSearchLocationString('');
+    // unfocus the input
+
+    // set the city as the current location in the store
+  };
 
   return (
     <View>
@@ -59,6 +103,37 @@ const SearchLocationsInput: React.FC<SearchLocationsInputProps> = ({
           </TouchableOpacity>
         ) : null}
       </View>
+
+      {showCitiesList && (
+        <View style={styles.searchResultsContainer}>
+          {citiesList.length >= 1 &&
+            citiesList.map((city: ICity, index: number) => (
+              <TouchableOpacity
+                key={`${city.lat}-${city.lon}`}
+                onPress={() => onSelectCity(city)}
+                style={[
+                  styles.searchResultItem,
+                  index === citiesList.length - 1 && {borderBottomWidth: 0},
+                ]}>
+                <Text style={styles.searchResultItemText}>
+                  {city.name}, {city.state || city.country}
+                </Text>
+              </TouchableOpacity>
+            ))}
+
+          {citiesList.length === 0 && (
+            <View style={{alignItems: 'center'}}>
+              <Text style={styles.searchResultItemText}>
+                No results found for {searchLocationString}
+              </Text>
+
+              <Text style={{textAlign: 'center', color: '#fff'}}>
+                Please try again with a different search string
+              </Text>
+            </View>
+          )}
+        </View>
+      )}
     </View>
   );
 };
@@ -80,5 +155,21 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     color: 'black',
+  },
+  searchResultsContainer: {
+    width: '90%',
+    marginTop: Metrics.spacing.l,
+    flexDirection: 'column',
+  },
+  searchResultItem: {
+    width: '100%',
+    paddingVertical: Metrics.spacing.l,
+    borderBottomColor: Colors.AVIVA_YELLOW,
+    borderBottomWidth: 1,
+  },
+  searchResultItemText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#fff',
   },
 });
