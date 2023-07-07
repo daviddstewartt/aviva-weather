bee01e343423e42269c83f3c767011edf5f4d59a
David Stewart <davidd.cc.stewartt@gmail.com>
2023-07-07 22:54:32 +0100

    Created: Base Home & Forecast Screens

---
+++ src/Screens/ForecastScreen/index.tsx
@@ -0,0 +1,16 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+type ForecastScreenProps = {};
+
+const ForecastScreen: React.FC<ForecastScreenProps> = () => {
+  return (
+    <View>
+      <Text>ForecastScreen</Text>
+    </View>
+  );
+};
+
+export default ForecastScreen;
+
+const styles = StyleSheet.create({});
---
+++ src/Screens/HomeScreen/index.tsx
@@ -0,0 +1,16 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+type HomeScreenProps = {};
+
+const HomeScreen: React.FC<HomeScreenProps> = () => {
+  return (
+    <View>
+      <Text>Homescreen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
