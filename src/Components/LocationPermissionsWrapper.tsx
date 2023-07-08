3622edbea549c20317d7f29efb989331d2b730a4
David Stewart <davidd.cc.stewartt@gmail.com>
2023-07-08 17:24:33 +0100

    Updated: Permission wrap 2 export set cur location

--- src/Components/LocationPermissionsWrapper.tsx
+++ src/Components/LocationPermissionsWrapper.tsx
@@ -1,38 +1,36 @@
 import {Platform} from 'react-native';
 import React, {PropsWithChildren, useEffect} from 'react';
+import {store} from '../redux/store';
 import {PERMISSIONS, request} from 'react-native-permissions';
 import {setLocationsPermissionsGranted} from '../redux/features/location';
-import {useDispatch} from 'react-redux';
 import {getUsersCurrentLocation} from '../util/location';
 
 type LocationPermissionsWrapperProps = {};
 
+export const requestLocationPermission = async () => {
+  try {
+    const PERMISSION = Platform.select({
+      ios: PERMISSIONS.IOS.LOCATION_WHEN_IN_USE,
+      android: PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+    });
+
+    const granted = await request(PERMISSION);
+
+    if (granted === 'granted') {
+      await getUsersCurrentLocation();
+      store.dispatch(setLocationsPermissionsGranted(true));
+    } else {
+      throw new Error('Location permission denied');
+    }
+  } catch (err) {
+    console.warn(err);
+    store.dispatch(setLocationsPermissionsGranted(false));
+  }
+};
+
 const LocationPermissionsWrapper: React.FC<
   LocationPermissionsWrapperProps & PropsWithChildren
 > = ({children}) => {
-  const dispatch = useDispatch();
-
-  const requestLocationPermission = async () => {
-    try {
-      const PERMISSION = Platform.select({
-        ios: PERMISSIONS.IOS.LOCATION_WHEN_IN_USE,
-        android: PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
-      });
-
-      const granted = await request(PERMISSION);
-
-      if (granted === 'granted') {
-        await getUsersCurrentLocation();
-        dispatch(setLocationsPermissionsGranted(true));
-      } else {
-        throw new Error('Location permission denied');
-      }
-    } catch (err) {
-      console.warn(err);
-      dispatch(setLocationsPermissionsGranted(false));
-    }
-  };
-
   useEffect(() => {
     requestLocationPermission();
   }, []);
