6890e0fae3d629ff57ebd41be109046ca5dcdd96
David Stewart <davidd.cc.stewartt@gmail.com>
2023-07-08 12:46:39 +0100

    Updated: Forecast Screen to fetch Forecast onload

--- src/Screens/ForecastScreen/index.tsx
+++ src/Screens/ForecastScreen/index.tsx
@@ -1,12 +1,41 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useSelector} from 'react-redux';
+import {RootState} from '../../redux/store';
+import {getSelectedCityForecast} from '../../util/location';
+import {Config} from '../../../config';
+import {IForecast} from '../../ts/interfaces';
 
 type ForecastScreenProps = {};
 
 const ForecastScreen: React.FC<ForecastScreenProps> = () => {
+  const {selectedCity} = useSelector((state: RootState) => state.location);
+  const [forecast, setForecast] = useState<IForecast | null>(null);
+
+  useEffect(() => {
+    if (selectedCity) {
+      // get forecast data
+      const fetchWeatherForecast = async () => {
+        try {
+          const selectedForecast = await getSelectedCityForecast(
+            Config.OPEN_WEATHER_MAP_API_KEY,
+          );
+          setForecast(selectedForecast);
+        } catch (error) {
+          console.log('error fetching forecast', error);
+        }
+      };
+
+      fetchWeatherForecast();
+    }
+  }, [selectedCity]);
+
   return (
     <View>
       <Text>ForecastScreen</Text>
+
+      <Text style={{marginTop: 200}}>Selected City: {selectedCity?.name}</Text>
+      <Text>forecast data: {JSON.stringify(forecast)}</Text>
     </View>
   );
 };
