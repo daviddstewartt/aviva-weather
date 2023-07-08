8ef01f321d5ecb4b29c38ed696caf530c81f4fcd
David Stewart <davidd.cc.stewartt@gmail.com>
2023-07-08 11:09:14 +0100

    Removed: Log

--- src/Screens/HomeScreen/index.tsx
+++ src/Screens/HomeScreen/index.tsx
@@ -1,12 +1,26 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {useSelector} from 'react-redux';
+import {RootState} from '../../redux/store';
 
 type HomeScreenProps = {};
 
 const HomeScreen: React.FC<HomeScreenProps> = () => {
+  const {selectedCity, currentLocation} = useSelector(
+    (state: RootState) => state.location,
+  );
+
+  /**
+   * @todo When permissions are denied, show a message to the user to go to settings
+   * @todo When theres no selectedCity, use the users current city
+   * @todo get the users current city
+   */
   return (
     <View>
       <Text>Homescreen</Text>
+      <Text style={{marginTop: 200}}>Selected City: {selectedCity?.name}</Text>
+      <Text>Current Location: {JSON.stringify(selectedCity)}</Text>
+      <Text>{JSON.stringify(currentLocation)}</Text>
     </View>
   );
 };
