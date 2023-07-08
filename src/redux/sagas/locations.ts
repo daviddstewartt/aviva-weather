78ac8237b2b8606b02474a77cb08442b5e780e54
David Stewart <davidd.cc.stewartt@gmail.com>
2023-07-08 11:02:47 +0100

    Updated: fetch CityWeather Saga

--- src/redux/sagas/locations.ts
+++ src/redux/sagas/locations.ts
@@ -1,10 +1,42 @@
-import {takeEvery, all} from 'redux-saga/effects';
-import {setSelectedCity} from '../features/location';
+import {all, takeLatest, call, put} from 'redux-saga/effects';
+import {PayloadAction} from '@reduxjs/toolkit';
+import {
+  requestSelectedCityWeather,
+  setSelectedCity,
+} from '../features/location';
+import {getCityWeather} from '../../util/location';
+import {ICity, ICityWithWeather} from '../../ts/interfaces';
+import {Config} from '../../../config';
 
-function* fetchCityWeatherData() {
-  console.log('fetchCityWeatherData');
+function* fetchCityWeatherData(action: PayloadAction<ICity>) {
+  try {
+    const cityWeather = yield call(
+      getCityWeather,
+      action.payload,
+      Config.OPEN_WEATHER_MAP_API_KEY,
+    );
+
+    // make Weather response compatibile with ICityWithWeather
+    delete cityWeather.coord;
+    delete cityWeather.name;
+
+    const newCityWeather: ICityWithWeather = {
+      ...action.payload,
+      weather: cityWeather,
+    };
+
+    yield put({type: setSelectedCity.type, payload: newCityWeather});
+  } catch (error) {
+    /**
+     * @todo handle error by creating setSelectedCityError action
+     */
+    console.error(error);
+    yield put({type: setSelectedCity.type, payload: null});
+  }
 }
 
 export default function* () {
-  yield all([yield takeEvery(setSelectedCity.type, fetchCityWeatherData)]);
+  yield all([
+    yield takeLatest(requestSelectedCityWeather.type, fetchCityWeatherData),
+  ]);
 }
