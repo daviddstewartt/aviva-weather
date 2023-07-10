38b32c3a2918e28ea55c22c275d9845cc226f0e5
David Stewart <davidd.cc.stewartt@gmail.com>
2023-07-10 11:28:43 +0100

    Added: Hourly Forecast Type

--- src/ts/interfaces.ts
+++ src/ts/interfaces.ts
@@ -127,3 +127,31 @@ export interface IForecast {
   rain: number;
   uvi: number;
 }
+
+export interface IForecastHourly {
+  dt: number;
+  temp: number;
+  feels_like: number;
+  pressure: number;
+  humidity: number;
+  dew_point: number;
+  uvi: number;
+  clouds: number;
+  visibility: number;
+  wind_speed: number;
+  wind_gust: number;
+  wind_deg: number;
+  pop: number;
+  rain?: {
+    '1h': number;
+  };
+  snow?: {
+    '1h': number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+}
