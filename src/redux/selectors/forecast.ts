8086329302773dc3338b2d686fe15890300a4678
David Stewart <davidd.cc.stewartt@gmail.com>
2023-07-10 13:00:01 +0100

    Added: Alerts & Current to forcast selectors

--- src/redux/selectors/forecast.ts
+++ src/redux/selectors/forecast.ts
@@ -1,7 +1,14 @@
-import {IForecastDaily, IForecastHourly} from '../../ts/interfaces';
+import {
+  IForecastAlert,
+  IForecastCurrent,
+  IForecastDaily,
+  IForecastHourly,
+} from '../../ts/interfaces';
 export interface ForecastState {
   hourly: IForecastHourly[] | null;
   daily: IForecastDaily[] | null;
+  current: IForecastCurrent | null;
+  alert: IForecastAlert | null;
   isLoading: boolean;
   error: string | null;
 }
@@ -9,6 +16,8 @@ export interface ForecastState {
 const initialState: ForecastState = {
   hourly: null,
   daily: null,
+  current: null,
+  alert: null,
   isLoading: false,
   error: null,
 };
