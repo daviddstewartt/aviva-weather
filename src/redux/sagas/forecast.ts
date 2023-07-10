6f99d378398f08c6a510c318c4576c0173038170
David Stewart <davidd.cc.stewartt@gmail.com>
2023-07-10 12:29:15 +0100

    Created: Forecast Saga after effect

---
+++ src/redux/sagas/forecast.ts
@@ -0,0 +1,30 @@
+import {all, takeLatest, call, put} from 'redux-saga/effects';
+import {PayloadAction} from '@reduxjs/toolkit';
+import {requestForecastData, setForecast, setError} from '../features/forecast';
+import {getCityForecast} from '../../util/forecast';
+import {ICity} from '../../ts/interfaces';
+import {Config} from '../../../config';
+
+function* fetchCityForecastData(action: PayloadAction<ICity>) {
+  console.log('fetchCityForecastData');
+
+  try {
+    const cityForecastData = yield call(
+      getCityForecast,
+      action.payload,
+      Config.OPEN_WEATHER_MAP_API_KEY,
+    );
+
+    yield put({type: setForecast.type, payload: cityForecastData});
+  } catch (error) {
+    console.error(error);
+    yield put({type: setForecast.type, payload: null});
+    yield put({type: setError.type, payload: error.message});
+  }
+}
+
+export default function* () {
+  yield all([
+    yield takeLatest(requestForecastData.type, fetchCityForecastData),
+  ]);
+}
