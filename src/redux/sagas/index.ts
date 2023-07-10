e0b71243d70635d1d3bd07b858bf4d4b1d25f7b9
David Stewart <davidd.cc.stewartt@gmail.com>
2023-07-10 12:29:29 +0100

    forecast saga

--- src/redux/sagas/index.ts
+++ src/redux/sagas/index.ts
@@ -2,8 +2,9 @@ import {all, fork} from 'redux-saga/effects';
 
 // Sagas
 import locationSaga from './locations';
+import forecastSaga from './forecast';
 
-const sagas = [locationSaga];
+const sagas = [locationSaga, forecastSaga];
 
 export default function* () {
   yield all(sagas.map(saga => fork(saga)));
