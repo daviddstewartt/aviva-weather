a78e476e5120a710565d10d96d8dbe386988b13d
David Stewart <davidd.cc.stewartt@gmail.com>
2023-07-11 12:02:38 +0100

    Added:Ability to use Vect Icon Lib 4 Weather icon

--- src/Components/WeatherIcon.tsx
+++ src/Components/WeatherIcon.tsx
@@ -1,24 +1,45 @@
 import {Image} from 'react-native';
 import React from 'react';
 import {isDayTime} from '../util/datetime';
+import {IconIdToVectorIconName} from '../data/Weather';
+import {Colors} from '../theme';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 type WeatherIconProps = {
   icon: string;
   width?: number;
   height?: number;
+  color?: string;
+  useVectorIcons?: boolean;
 };
-/** @todo use react-native-icons and create relations between OpenWeatherMap codes and icons */
-const WeatherIcon: React.FC<WeatherIconProps> = ({icon, width, height}) => {
+const WeatherIcon: React.FC<WeatherIconProps> = ({
+  icon,
+  width,
+  height,
+  color,
+  useVectorIcons,
+}) => {
   const getImageURI = (): string => {
     const dir = isDayTime() ? 'w' : 'wn'; // w for day icons, wn for night icons
     return `http://openweathermap.org/img/${dir}/${icon}.png`;
   };
-  return <Image source={{uri: getImageURI()}} style={{width, height}} />;
+
+  let iconSource = useVectorIcons
+    ? IconIdToVectorIconName(icon)
+    : getImageURI();
+  if (useVectorIcons && iconSource) {
+    return (
+      <MaterialCommunityIcons name={iconSource} size={width} color={color} />
+    );
+  }
+  return <Image source={{uri: iconSource}} style={{width, height}} />;
 };
 
 WeatherIcon.defaultProps = {
   width: 50,
   height: 50,
+  color: Colors.DARK,
+  useVectorIcons: true,
 };
 
 export default WeatherIcon;
